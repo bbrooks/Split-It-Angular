5215302662906543dd93a35db8193fc6d1683896
Ben Brooks <ben@benbrooks.net>
2013-10-12 18:19:33 -0700

    Added an end-to-end test for ability to add a transaction

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,44 +2,32 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('my app', function() {
+describe('Split It', function() {
 
   beforeEach(function() {
     browser().navigateTo('../../app/index.html');
   });
 
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser().location().url()).toBe("/view1");
+  it('should be able to add a transaction', function(){
+
+    // Remove all initial transactions
+    // @Todo: figure out how to access scope and do this better
+    element('.transaction a[ng-click="deleteTransaction(t)"]').click();
+
+    // Add a transaction
+    select('newTransaction.lender').option(1);
+    input('newTransaction.name').enter('A really unlikely purchase');
+    input('newTransaction.amount').enter(324);
+    element('#add-transaction-form button[type="submit"]').click();
+
+    var transaction_count = repeater('#transactions .transaction').count();
+
+
+    expect( transaction_count ).toEqual( 1 );
+
+
   });
 
 
-  describe('view1', function() {
-
-    beforeEach(function() {
-      browser().navigateTo('#/view1');
-    });
-
-
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 1/);
-    });
-
-  });
-
-
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser().navigateTo('#/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 2/);
-    });
-
-  });
 });
