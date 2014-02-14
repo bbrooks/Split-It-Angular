913f7c87541aee57ce87961415cad68d9870cc8f
Ben Brooks <ben@benbrooks.net>
2014-02-14 15:52:08 -0800

    defaults and validation for reusable 'purchase' form

--- app/scripts/directives/purchaseform.js
+++ app/scripts/directives/purchaseform.js
@@ -2,38 +2,71 @@
 
 angular.module('splitItApp')
 
-.controller('PurchaseFormCtrl', function($scope, peopleService){
-
-	$scope.people = peopleService.people;
-	$scope.purchase = {}
-
-	$scope.addPurchase = function(newPurchase){
-		var sanitizedPurchase = sanitize( newPurchase );
-		$scope.purchase = sanitizedPurchase;
-	};
-
-	var sanitize = function( purchase ){
-		var cleanPurchase = {};
-
-		//Check for neccessary fields
-		cleanPurchase.description = purchase.description;
-		cleanPurchase.purchaser = purchase.payer.uuid;
-		cleanPurchase.splitBetween = _.pluck(purchase.owers, 'uuid');
-		cleanPurchase.purchaseDate = new Date(purchase.date);
-		cleanPurchase.cost = parseFloat(purchase.amount);
-
-		return cleanPurchase;
-	};
-
-})	
-
 .directive('purchaseForm', function () {
 	return {
 		templateUrl: 'views/purchaseform.html',
 		restrict: 'E',
 		scope: {
-			submit: '@submit'
+			submitLabel: '@',
+			purchase: '@'
 		},
 		controller: 'PurchaseFormCtrl'
 	};
+})
+
+.controller('PurchaseFormCtrl', function($scope, peopleService){
+
+	$scope.people = peopleService.people;
+
+	var purchaseFormDefaultState = {
+		date: getTodayStr(),
+		owers: _.pluck(peopleService.people, 'uuid')
+	};
+	
+	// Set Defaults
+	$scope.newPurchase = angular.copy(purchaseFormDefaultState);
+
+	$scope.addPurchase = function(newPurchase){
+		$scope.purchase = buildPurchase( newPurchase );
+
+		// Reset Form
+		$scope.newPurchase = angular.copy(purchaseFormDefaultState);
+		$scope.purchaseForm.$setPristine();
+	};
+
+	function buildPurchase( purchase ){
+		var cleanPurchase = {};
+
+		//Check for neccessary fields
+		var required_attrs = [
+			'description',
+			'payer',
+			'owers',
+			'date',
+			'amount'
+		];
+
+		// @Todo: check for required attrs.
+
+		// Assign data to model
+		cleanPurchase.description = purchase.description;
+		cleanPurchase.purchaser = purchase.payer;
+		cleanPurchase.splitBetween = purchase.owers;
+		cleanPurchase.purchaseDate = new Date(purchase.date);
+		cleanPurchase.cost = parseFloat(purchase.amount);
+
+		return cleanPurchase;
+	}
+
+	function getTodayStr(){
+		var today = new Date();
+		var dd = today.getDate();
+		var mm = today.getMonth()+1; //January is 0!
+
+		var yyyy = today.getFullYear();
+		if(dd<10){dd='0'+dd} if(mm<10){mm='0'+mm} today = yyyy+'-'+mm+'-'+dd;
+
+		return today;
+	}
+
 });
