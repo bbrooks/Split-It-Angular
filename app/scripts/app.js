9624060fb51b8d45e5f623e5aeb864ff7ec059e2
Ben Brooks <ben@benbrooks.net>
2014-02-24 16:54:37 -0800

    Really simple loading notification

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,4 +20,29 @@ angular.module('splitItApp', [
       .otherwise({
         redirectTo: '/'
       });
-  });
+  }) // route config
+
+  .config(function($httpProvider){
+    $httpProvider.interceptors.push(function($q, $rootScope, $timeout){
+      return{
+        request: function(config){
+          $rootScope.notification = '<div class="alert alert-info">Loading...</div>';
+          return config || $q.when(config);
+        },
+        requestError: function(rejection){
+          $rootScope.notification = '<div class="alert alert-warning">There was an error</div>';
+          $timeout(function(){$rootScope.notification = '';}, 1000);
+          return $q.reject(rejection);
+        },
+        response: function(response){
+          $timeout(function(){$rootScope.notification = '';}, 500);
+          return response || $q.when(response);
+        },
+        responseError: function(rejection){
+          return $q.reject(rejection);
+          $rootScope.notification = '<div class="alert alert-warning">There was an error</div>';
+          $timeout(function(){$rootScope.notification = '';}, 1000);
+        },
+      };
+    });
+  }); // http interceptor config
